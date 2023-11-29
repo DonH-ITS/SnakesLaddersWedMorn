6c18532b071e9c0343df4b6374185a0e025cfb69
donny.hurley@atu.ie <donny.hurley@atu.ie@A-CMLMNQ3>
2023-11-29 09:38:46 +0000

    frames added

--- SnakesLaddersWedMorn/MainPage.xaml.cs
+++ SnakesLaddersWedMorn/MainPage.xaml.cs
@@ -4,6 +4,25 @@
     {
         public MainPage() {
             InitializeComponent();
+            CreatetheGrid();
+        }
+
+        private void CreatetheGrid() {
+            for (int i = 0; i < 10; ++i) {
+                for (int j = 0; j < 10; ++j) {
+                    Frame styledFrame = new()
+                    {
+                        BackgroundColor = Color.FromRgb(255, 255, 255), // Set the background color
+                        CornerRadius = 10, // Set rounded corners
+                        HasShadow = false, // Add a shadow effect
+                        Padding = new Thickness(0), // Add padding to the frame
+                        BorderColor = Color.FromRgb(100, 100, 100)
+
+                    };
+                    GameBoard.Add(styledFrame, j, i);
+                }
+
+            }
         }
     }
 }
