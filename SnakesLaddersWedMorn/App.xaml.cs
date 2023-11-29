70b681084f124ecac1d4df65f30846dd2d322f6a
donny.hurley@atu.ie <donny.hurley@atu.ie@A-CMLMNQ3>
2023-11-29 09:18:38 +0000

    Add project files.

---
+++ SnakesLaddersWedMorn/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SnakesLaddersWedMorn
+{
+    public partial class App : Application
+    {
+        public App() {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SnakesLaddersWedMorn/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SnakesLaddersWedMorn
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell() {
+            InitializeComponent();
+        }
+    }
+}
