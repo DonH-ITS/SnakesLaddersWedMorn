8b0f2f117f11eeb80cbecc35f0ee4cc926fbc038
donny.hurley@atu.ie <donny.hurley@atu.ie@A-CMLMNQ3>
2023-12-06 09:25:48 +0000

    player class

---
+++ SnakesLaddersWedMorn/Player.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace SnakesLaddersWedMorn
+{
+    public class Player
+    {
+        private int position;
+        private int row;
+        private int col;
+        private Image image;
+        static private Grid grid;
+
+        public Player(Image img) {
+            row = 9;
+            col = 0;
+            position = 1;
+            this.image = img;
+        }
+    }
+}
